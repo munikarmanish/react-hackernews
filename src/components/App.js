3c60ecf8e599cfd902ef5ed8014453a1bf1c4895
Manish Munikar <munikarmanish@gmail.com>
2017-11-17 15:44:19 +0545

    create link form works

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import LinkList from './LinkList';
+import CreateLink from './CreateLink';
 
 class App extends React.Component {
   render() {
     return (
-      <LinkList />
+      // <LinkList />
+      <CreateLink />
     );
   }
 }
---
+++ src/components/CreateLink.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+
+class CreateLink extends React.Component {
+
+  state = {
+    description: '',
+    url: '',
+  };
+
+  _createLink = async () => {
+    const { description, url } = this.state;
+    await this.props.createLinkMutation({
+      variables: { description, url }
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="flex flex-column mt3">
+
+          <input
+            className="mb2"
+            value={this.state.description}
+            onChange={e => this.setState({ description: e.target.value })}
+            type="text"
+            placeholder="A description for the link"
+          />
+
+          <input
+            className="mb2"
+            value={this.state.url}
+            onChange={e => this.setState({ url: e.target.value })}
+            type="text"
+            placeholder="The URL for the link"
+          />
+
+        </div>
+
+        <button onClick={() => this._createLink()}>
+          Submit
+        </button>
+
+      </div>
+    );
+  }
+}
+
+const CREATE_LINK_MUTATION = gql`
+  mutation CreateLinkMutation($description: String!, $url: String!) {
+    createLink(description: $description, url: $url) {
+      id
+      createdAt
+      description
+      url
+    }
+  }
+`;
+
+export default graphql(CREATE_LINK_MUTATION, { name: 'createLinkMutation' })(CreateLink);
